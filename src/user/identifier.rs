aad6f34d3216e00be7cf6f2b71290ce750a90918
Laysar <laysar.main@gmail.com>
2025-07-03 16:14:50 -0400

    Added From implementations for `UserIdentifier`

--- src/user/identifier.rs
+++ src/user/identifier.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, num::NonZeroU64};
 
 use serde::Serialize;
 use uuid::Uuid;
@@ -13,6 +13,28 @@ pub enum UserIdentifier<'a> {
 	/// User's discord snowflake id
 	DiscordId(u64),
 }
+
+impl From<u64> for UserIdentifier<'_> {
+	fn from(value: u64) -> Self {
+		Self::DiscordId(value)
+	}
+}
+impl From<NonZeroU64> for UserIdentifier<'_> {
+	fn from(value: NonZeroU64) -> Self {
+		Self::DiscordId(value.get())
+	}
+}
+impl From<Uuid> for UserIdentifier<'_> {
+	fn from(value: Uuid) -> Self {
+		Self::Uuid(value)
+	}
+}
+impl<'a> From<&'a str> for UserIdentifier<'a> {
+	fn from(value: &'a str) -> Self {
+		Self::Nickname(value)
+	}
+}
+
 impl Display for UserIdentifier<'_> {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		match &self {
